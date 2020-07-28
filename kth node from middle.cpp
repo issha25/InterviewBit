2604177253aa0b0506c6b65e04154cc3e5d19a95
issha25 <68890096+issha25@users.noreply.github.com>
2020-07-28 11:48:58 +0530

    Update kth node from middle.cpp

--- kth node from middle.cpp
+++ kth node from middle.cpp
@@ -6,42 +6,21 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-ListNode* Solution::reverseBetween(ListNode* A, int B, int C) {
-    if (A == NULL)
-        return A;
-
-    ListNode* curr = A;
-    ListNode* prev = NULL;
-
-    while (B > 1) {
-        prev = curr;
-        curr = curr->next;
-        B--;
-        C--;
+int Solution::solve(ListNode* A, int B) {
+ListNode* head=A;
+int ans=0;
+while(head!=NULL){
+    ans++;
+    head=head->next;
+}
+int mid=(ans/2)+1;
+int f=mid-B;
+if(f>0){
+while (--f){
+    A=A->next;
     }
-
-    ListNode* conn = prev;
-    ListNode* tail = curr;
-
-    ListNode* temp = NULL;
-
-    while (C > 0) {
-        temp = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = temp;
-        C--;
-    }
-
-    if (conn != NULL) {       //linked list k phle koi nos the check krne k liye
-        conn->next = prev;
-    }
-    else {
-        A = prev;
-    }
-
-    tail->next = curr;
-
-    return A;
-
+return A->val;
+}
+else
+    return -1;
 }
