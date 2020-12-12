32c3248e0b7f69d043462d2548e5a9633d513659
Isha Gupta <68890096+issha25@users.noreply.github.com>
2020-12-12 23:36:36 +0530

    Create Single Number.cpp

---
+++ Single Number.cpp
@@ -0,0 +1,14 @@
+int Solution::singleNumber(const vector<int> &A) {
+
+     if(A.size() == 0){
+        return 0;
+    }
+    
+    int number = A[0];
+    for(int i = 1; i < A.size(); i++){
+        number = number ^ A[i];
+    }
+    
+    return number;
+}
+
